399b218d3ad023dbf0573a45e4f0509581f8b6f6
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-08-04 20:24:47 +0700

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -30,14 +30,14 @@ echo -e  "_____________________________________________________________"
 ###################################################
 echo -e "mohon tunggu sebentar"
 echo -e  "╭━━━━━━━╮"
-echo -e  "┃  ● ══  ┃"
-echo -e  "┃███████┃"  echo -e "─╤╦︻=(◣_◢)=︻╦╤─"
+echo -e  "┃  ● ══ ┃"
+echo -e  "┃███████┃"    echo -e "─╤╦︻=(◣_◢)=︻╦╤─"
 echo -e  "┃███████┃"
-echo -e  "┃███████┃"  echo -e  "==[ INSTALL TOOLS  PRIBADI ]==" 
-echo -e  "┃███████┃"  echo -e  "+-----------------------------+"
-echo -e  "┃███████┃"  echo -e  "| Author    : Wisnu Agung.S   |" 
-echo -e  "┃   ○    ┃"  echo -e  "| instagram : wisnu_as123     |" 
-echo -e  "╰━━━━━━━╯"  echo -e  "+-----------------------------+"
+echo -e  "┃███████┃"           "==[ INSTALL TOOLS  PRIBADI ]==" 
+echo -e  "┃███████┃"           "+-----------------------------+"
+echo -e  "┃███████┃"           "| Author    : Wisnu Agung.S   |" 
+echo -e  "┃   ○   ┃"            "| instagram : wisnu_as123     |" 
+echo -e  "╰━━━━━━━╯"           "+-----------------------------+"
 
 trap ctrl_c INT
 ctrl_c() {
